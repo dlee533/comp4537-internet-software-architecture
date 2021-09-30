408e78dde06a54cdf6f0e9df59cc1810d5c66e4c
dongl <dmlee533@gmail.com>
2021-09-29 18:10:12 -0700

    lab3 part2

---
+++ COMP4537/labs/3/getDate/modules/utils.js
@@ -0,0 +1,4 @@
+exports.getDate =
+function() {
+  return `Server current date and time is ${new Date()}`;
+}
---
+++ COMP4537/labs/3/getDate/server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const url = require('url');
+const util = require('./modules/utils');
+
+http.createServer(function (req, res) {
+  const q = url.parse(req.url, true);
+  res.writeHead(200, {'Content-Type': 'text/html', 'Access-Control-Allow-Origin': '*'});
+  res.end(`<p style="color:blue">Hello ${q.query.name}, What a beautiful day. ${util.getDate()}<p>`);
+}).listen(8888);
+//change the port number
+
+console.log("server is running and listening");
